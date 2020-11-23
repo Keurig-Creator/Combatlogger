d298b55b3d486c0867449de7e76353cf05a4bbbe
Keurig-Creator <threetwoone112@gmail.com>
2020-11-22 21:23:29 -0600

    using lombok

--- src/main/java/com/keurig/combatlogger/handler/CombatPlayer.java
+++ src/main/java/com/keurig/combatlogger/handler/CombatPlayer.java
@@ -1,29 +1,28 @@
 package com.keurig.combatlogger.handler;
 
 import com.keurig.combatlogger.CombatLogger;
+import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.*;
 
+@Getter
 public class CombatPlayer {
 
 	private final CombatLogger main;
 
 	private final Map<UUID, Long> combatLogged;
-
 	private final Set<UUID> players;
-
 	private final Map<UUID, Integer> task;
 
 	public CombatPlayer(CombatLogger main) {
 		this.main = main;
 
-		combatLogged = new HashMap<UUID, Long>();
-		task = new HashMap<UUID, Integer>();
-
-		players = new HashSet<UUID>();
+		this.combatLogged = new HashMap<UUID, Long>();
+		this.task = new HashMap<UUID, Integer>();
+		this.players = new HashSet<UUID>();
 
 		addOnlinePlayers();
 	}
@@ -31,50 +30,50 @@ public class CombatPlayer {
 	public void addCombat(final Player player, final Player target) {
 
 		// Get combat message
-		final String inCombat = main.getConfig().getString("combat-message");
+		final String inCombat = this.main.getConfig().getString("combat-message");
 		assert inCombat != null;
 
-		if (combatLogged.containsKey(player.getUniqueId()) && combatLogged.get(player.getUniqueId()) > System.currentTimeMillis()) {
-			Bukkit.getScheduler().cancelTask(task.get(player.getUniqueId()));
+		if (this.combatLogged.containsKey(player.getUniqueId()) && this.combatLogged.get(player.getUniqueId()) > System.currentTimeMillis()) {
+			Bukkit.getScheduler().cancelTask(this.task.get(player.getUniqueId()));
 		} else {
 			if (!player.hasPermission("combatlogger.admin"))
 				player.sendMessage(ChatColor.translateAlternateColorCodes('&', inCombat));
 		}
 
-		if (combatLogged.containsKey(target.getUniqueId()) && combatLogged.get(target.getUniqueId()) > System.currentTimeMillis()) {
-			Bukkit.getScheduler().cancelTask(task.get(target.getUniqueId()));
+		if (this.combatLogged.containsKey(target.getUniqueId()) && this.combatLogged.get(target.getUniqueId()) > System.currentTimeMillis()) {
+			Bukkit.getScheduler().cancelTask(this.task.get(target.getUniqueId()));
 		} else {
 			if (!target.hasPermission("combatlogger.admin"))
 				target.sendMessage(ChatColor.translateAlternateColorCodes('&', inCombat));
 		}
 
 		// Get combat timer
-		final int combatTimer = main.getConfig().getInt("combat-timer");
+		final int combatTimer = this.main.getConfig().getInt("combat-timer");
 
 		// Get combat off message
-		final String outOfCombat = main.getConfig().getString("combat-off-message");
+		final String outOfCombat = this.main.getConfig().getString("combat-off-message");
 		assert outOfCombat != null;
 
 		// Add attacker and target to combatLogged
 		if (!player.hasPermission("combatlogger.admin")) {
-			combatLogged.put(player.getUniqueId(), System.currentTimeMillis() + (combatTimer * 1000));
+			this.combatLogged.put(player.getUniqueId(), System.currentTimeMillis() + (combatTimer * 1000));
 
-			task.put(player.getUniqueId(), Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+			this.task.put(player.getUniqueId(), Bukkit.getScheduler().runTaskLater(this.main, new Runnable() {
 				public void run() {
-					if (combatLogged.containsKey(player.getUniqueId())) {
-						combatLogged.remove(player.getUniqueId());
+					if (CombatPlayer.this.combatLogged.containsKey(player.getUniqueId())) {
+						CombatPlayer.this.combatLogged.remove(player.getUniqueId());
 						player.sendMessage(ChatColor.translateAlternateColorCodes('&', outOfCombat));
 					}
 				}
 			}, 20 * combatTimer).getTaskId());
 		}
 		if (!target.hasPermission("combatlogger.admin")) {
-			combatLogged.put(target.getUniqueId(), System.currentTimeMillis() + (combatTimer * 1000));
+			this.combatLogged.put(target.getUniqueId(), System.currentTimeMillis() + (combatTimer * 1000));
 
-			task.put(target.getUniqueId(), Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+			this.task.put(target.getUniqueId(), Bukkit.getScheduler().runTaskLater(this.main, new Runnable() {
 				public void run() {
-					if (combatLogged.containsKey(target.getUniqueId())) {
-						combatLogged.remove(target.getUniqueId());
+					if (CombatPlayer.this.combatLogged.containsKey(target.getUniqueId())) {
+						CombatPlayer.this.combatLogged.remove(target.getUniqueId());
 						target.sendMessage(ChatColor.translateAlternateColorCodes('&', outOfCombat));
 					}
 				}
@@ -85,42 +84,27 @@ public class CombatPlayer {
 
 	public void addOnlinePlayers() {
 		for (Player player : Bukkit.getOnlinePlayers()) {
-			if (!players.contains(player.getUniqueId()))
-				players.add(player.getUniqueId());
+			if (!this.players.contains(player.getUniqueId()))
+				this.players.add(player.getUniqueId());
 		}
 	}
 
 	public void addPlayer(Player player) {
-		if (!players.contains(player.getUniqueId()))
-			players.add(player.getUniqueId());
+		if (!this.players.contains(player.getUniqueId()))
+			this.players.add(player.getUniqueId());
 	}
 
 	public void removePlayer(Player player) {
 
-		Bukkit.getScheduler().cancelTask(task.get(player.getUniqueId()));
+		Bukkit.getScheduler().cancelTask(this.task.get(player.getUniqueId()));
 
-		if (combatLogged.containsKey(player.getUniqueId())) {
-			combatLogged.remove(player.getUniqueId());
+		if (this.combatLogged.containsKey(player.getUniqueId())) {
+			this.combatLogged.remove(player.getUniqueId());
 		}
 
-		if (players.contains(player.getUniqueId())) {
-			players.remove(player.getUniqueId());
+		if (this.players.contains(player.getUniqueId())) {
+			this.players.remove(player.getUniqueId());
 		}
 
-	}
-
-	public Player getPlayer(Player player) {
-		if (players.contains(player.getUniqueId())) {
-			return player;
-		}
-		return null;
-	}
-
-	public Set<UUID> getPlayers() {
-		return players;
-	}
-
-	public Map<UUID, Long> getCombatLogged() {
-		return combatLogged;
 	}
 }
--- src/main/java/com/keurig/combatlogger/listeners/JoinListener.java
+++ src/main/java/com/keurig/combatlogger/listeners/JoinListener.java
@@ -20,7 +20,7 @@ public class JoinListener implements Listener {
 
 	private final CombatLogger main;
 
-	private Map<UUID, Long> banned = new HashMap<UUID, Long>();
+	private final Map<UUID, Long> banned = new HashMap<UUID, Long>();
 
 	public JoinListener(CombatLogger main) {
 		this.main = main;
@@ -28,14 +28,15 @@ public class JoinListener implements Listener {
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
-		main.getCombatPlayer().addPlayer(e.getPlayer());
+		this.main.getCombatPlayer().addPlayer(e.getPlayer());
 	}
 
 	@EventHandler
 	public void onPreJoin(AsyncPlayerPreLoginEvent e) {
-		if (banned.containsKey(e.getUniqueId()) && banned.get(e.getUniqueId()) > System.currentTimeMillis()) {
 
-			final List<String> punishment1 = main.getConfig().getStringList("punishment");
+		if (this.banned.containsKey(e.getUniqueId()) && this.banned.get(e.getUniqueId()) > System.currentTimeMillis()) {
+
+			final List<String> punishment1 = this.main.getConfig().getStringList("punishment");
 
 			for (int i = 0; i < punishment1.size(); i++) {
 
@@ -43,24 +44,24 @@ public class JoinListener implements Listener {
 					final String[] args = punishment1.get(i).split(":");
 
 					if (PunishmentTypes.valueOf(args[0]) == PunishmentTypes.BAN) {
-						final int timeRemaining = (int) ((banned.get(e.getUniqueId()) - System.currentTimeMillis()) / 1000);
+						final int timeRemaining = (int) ((this.banned.get(e.getUniqueId()) - System.currentTimeMillis()) / 1000);
 
 						e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_BANNED, ChatColor.translateAlternateColorCodes('&', args[2]).replace("{timeRemaining}",
 								String.valueOf(timeRemaining)));
 					}
 				}
-				
+
 			}
 		}
 	}
 
 	@EventHandler
 	public void onQuit(PlayerQuitEvent e) {
-		final CombatPlayer combatPlayer = main.getCombatPlayer();
+		final CombatPlayer combatPlayer = this.main.getCombatPlayer();
 
 		Player player = e.getPlayer();
 
-		final List<String> punishment1 = main.getConfig().getStringList("punishment");
+		final List<String> punishment1 = this.main.getConfig().getStringList("punishment");
 
 		if (e.getPlayer().hasPermission("combatlogger.admin"))
 			return;
@@ -72,7 +73,7 @@ public class JoinListener implements Listener {
 					final String[] args = punishment1.get(i).split(":");
 
 					if (PunishmentTypes.valueOf(args[0]) == PunishmentTypes.BAN) {
-						banned.put(player.getUniqueId(), System.currentTimeMillis() + Long.parseLong(args[1]) * 1000);
+						this.banned.put(player.getUniqueId(), System.currentTimeMillis() + Long.parseLong(args[1]) * 1000);
 					}
 				} else {
 					if (PunishmentTypes.valueOf(punishment1.get(i)) == PunishmentTypes.KILL) {
@@ -85,4 +86,10 @@ public class JoinListener implements Listener {
 			combatPlayer.removePlayer(player);
 		}
 	}
+
+//	public String getPunishment() {
+//		final List<String> punishment1 = main.getConfig().getStringList("punishment");
+//
+//	}
+
 }
