305063e9833220b2321591d96c180ab4afce9f99
Keurig <64445183+Keurig-Creator@users.noreply.github.com>
2021-11-21 23:34:31 -0600

    Added timeformat

--- src/main/java/com/keurig/combatlogger/punishment/punishments/BanPunishment.java
+++ src/main/java/com/keurig/combatlogger/punishment/punishments/BanPunishment.java
@@ -1,6 +1,7 @@
 package com.keurig.combatlogger.punishment.punishments;
 
 import com.keurig.combatlogger.punishment.Punishment;
+import com.keurig.combatlogger.utils.Chat;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,44 +13,31 @@ import java.util.UUID;
 
 public class BanPunishment extends Punishment {
 
-	private String[] args;
+    private String[] args;
 
-	private final Map<UUID, Long> banned;
+    private final Map<UUID, Long> banned;
 
-	public BanPunishment() {
-		super("BAN", 2);
+    public BanPunishment() {
+        super("BAN", 2);
 
-		this.banned = new HashMap<>();
-	}
+        this.banned = new HashMap<>();
+    }
 
-	@EventHandler
-	public void onPreJoin(PlayerPreLoginEvent event) {
-		if (this.banned.containsKey(event.getUniqueId()) && this.banned.get(event.getUniqueId()) > System.currentTimeMillis())
-			event.disallow(PlayerPreLoginEvent.Result.KICK_BANNED, ChatColor.translateAlternateColorCodes('&',
-					this.args[1].replace("{timeRemaining}", timeFormat(this.banned.get(event.getUniqueId()) - System.currentTimeMillis()))));
-	}
+    @EventHandler
+    public void onPreJoin(PlayerPreLoginEvent event) {
+        if (this.banned.containsKey(event.getUniqueId()) && this.banned.get(event.getUniqueId()) > System.currentTimeMillis())
+            event.disallow(PlayerPreLoginEvent.Result.KICK_BANNED, ChatColor.translateAlternateColorCodes('&',
+                    this.args[1].replace("{timeRemaining}", Chat.timeFormat(this.banned.get(event.getUniqueId()) - System.currentTimeMillis()))));
+    }
 
-	@Override
-	public void onQuit(String label, String[] args) {
-		final Player player = getPlayer();
+    @Override
+    public void onQuit(String label, String[] args) {
+        final Player player = getPlayer();
 
-		if (player.hasPermission("combatlogger.admin"))
-			return;
+        if (player.hasPermission("combatlogger.admin"))
+            return;
 
-		this.args = args;
-		this.banned.put(player.getUniqueId(), System.currentTimeMillis() + (Integer.parseInt(args[0]) * 1000));
-	}
-
-	private String timeFormat(long milis) {
-
-		long second = (milis / 1000) % 60;
-		long minute = (milis / (1000 * 60)) % 60;
-
-		if (minute > 0) {
-			return String.format("%dm %ds", minute, second);
-		} else if (second > 0) {
-			return String.format("%ds", second);
-		} else
-			return String.format("%dms", milis);
-	}
+        this.args = args;
+        this.banned.put(player.getUniqueId(), System.currentTimeMillis() + (Integer.parseInt(args[0]) * 1000));
+    }
 }
--- src/main/java/com/keurig/combatlogger/utils/Chat.java
+++ src/main/java/com/keurig/combatlogger/utils/Chat.java
@@ -9,19 +9,32 @@ import java.util.Arrays;
 
 public class Chat {
 
-	public static String color(String message) {
-		return ChatColor.translateAlternateColorCodes('&', message);
-	}
+    public static String color(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
 
-	public static void message(Player player, String... messages) {
-		Arrays.stream(messages).forEach(player::sendMessage);
-	}
+    public static void message(Player player, String... messages) {
+        Arrays.stream(messages).forEach(player::sendMessage);
+    }
 
-	public static void message(CommandSender sender, String... messages) {
-		Arrays.stream(messages).forEach(sender::sendMessage);
-	}
+    public static void message(CommandSender sender, String... messages) {
+        Arrays.stream(messages).forEach(sender::sendMessage);
+    }
 
-	public static void log(String message) {
-		Bukkit.getConsoleSender().sendMessage(color(message));
-	}
+    public static void log(String message) {
+        Bukkit.getConsoleSender().sendMessage(color(message));
+    }
+
+    public static String timeFormat(long milis) {
+
+        long second = (milis / 1000) % 60;
+        long minute = (milis / (1000 * 60)) % 60;
+
+        if (minute > 0) {
+            return String.format("%dm %ds", minute, second);
+        } else if (second > 0) {
+            return String.format("%ds", second);
+        } else
+            return String.format("%dms", milis);
+    }
 }
