37a63840aaf728fd1d8322553411f161e1829634
Keurig <64445183+Keurig-Creator@users.noreply.github.com>
2021-12-03 19:56:13 -0600

    Update CombatRemoveRunnable.java

--- src/main/java/com/keurig/combatlogger/runnables/CombatRemoveRunnable.java
+++ src/main/java/com/keurig/combatlogger/runnables/CombatRemoveRunnable.java
@@ -1,18 +1,23 @@
 package com.keurig.combatlogger.runnables;
 
-import com.keurig.combatlogger.CombatLoggerPlugin;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Map;
+import java.util.UUID;
+
 public class CombatRemoveRunnable extends BukkitRunnable {
+
+    private final Map<UUID, CombatRemoveRunnable> loggedPlayers;
 
     private final Player player;
 
     @Getter
     private final Player target;
 
-    public CombatRemoveRunnable(Player player, Player target) {
+    public CombatRemoveRunnable(Map<UUID, CombatRemoveRunnable> loggedPlayers, Player player, Player target) {
+        this.loggedPlayers = loggedPlayers;
         this.player = player;
         this.target = target;
     }
@@ -20,5 +25,12 @@ public class CombatRemoveRunnable extends BukkitRunnable {
     @Override
     public void run() {
         player.sendMessage("CXOMBAT REMOVED HAHAHAHA");
+        cancel();
+    }
+
+    @Override
+    public synchronized void cancel() throws IllegalStateException {
+        loggedPlayers.remove(player.getUniqueId());
+        super.cancel();
     }
 }
