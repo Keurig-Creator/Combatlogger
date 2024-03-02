1033a2821f0fba90d23cf4c2acf43bba771dc2ae
David <threetwoone112@gmail.com>
2024-03-01 18:06:28 -0600

    reordered how argument check works. removed annoying log

--- src/main/java/com/keurig/combatlogger/punishment/PunishmentManager.java
+++ src/main/java/com/keurig/combatlogger/punishment/PunishmentManager.java
@@ -65,28 +65,27 @@ public class PunishmentManager {
         final List<String> punishmentArgs = plugin.getConfig().getStringList("punishment");
 
         for (String punishmentArg : punishmentArgs) {
-            final String[] split = punishmentArg.split(":");
-            final String label = split[0];
-            String[] args = new String[0];
+            final String[] tempSplit = punishmentArg.split(":");
+            final String label = tempSplit[0];
 
-            if (getPunishmentByName(label) == null) {
+            Punishment punishment = getPunishmentByName(label);
+
+            if (punishment == null) {
                 plugin.getLogger().log(Level.WARNING, "Cannot find " + label + " punishment! Did you misspell it or forgot to register it?");
                 continue;
             }
 
-            final Punishment punishment = getPunishmentByName(label);
             punishment.setPlayer(player);
 
-            if (punishment.getNumberArgs() >= 1) {
-                args = Arrays.copyOfRange(split, 1, split.length);
+            // Filtered out args basically keeps the other : if you have messages or what not
+            // it would get removed from the split so were just checking the number of args first
+            String[] args = new String[0];
 
-                if (punishment.getNumberArgs() != args.length) {
-                    plugin.getLogger().log(Level.WARNING, punishment.getName() + ", must have " + punishment.getNumberArgs() + " argument" + (punishment.getNumberArgs() == 1 ? "" : "s") + "!");
-                    continue;
-                }
-            } else if (split.length >= 2) {
-                plugin.getLogger().log(Level.WARNING, punishment.getName() + " Has no arguments!");
-                continue;
+            final String[] split = punishmentArg.split(":", punishment.getNumberArgs() + 1);
+
+            if (punishment.getNumberArgs() >= 1) {
+                // Check if the punishment specifies a minimum number of arguments
+                args = Arrays.copyOfRange(split, 1, split.length);
             }
 
             punishment.onQuit(label, args);
