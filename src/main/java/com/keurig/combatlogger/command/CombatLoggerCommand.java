266db63e789ba2914bc780780d9e09843443f2f1
David <threetwoone112@gmail.com>
2024-03-01 19:21:17 -0600

    added info command

--- src/main/java/com/keurig/combatlogger/command/CombatLoggerCommand.java
+++ src/main/java/com/keurig/combatlogger/command/CombatLoggerCommand.java
@@ -2,6 +2,7 @@ package com.keurig.combatlogger.command;
 
 import com.keurig.combatlogger.CombatLogger;
 import com.keurig.combatlogger.utils.Chat;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -29,15 +30,28 @@ public class CombatLoggerCommand implements CommandExecutor, TabCompleter {
 
             if (type.equalsIgnoreCase("reload") || type.equalsIgnoreCase("rl")) {
                 plugin.reloadConfig();
-                
+
                 HandlerList.unregisterAll(plugin);
                 CombatLogger.getInstance().registerEvents();
+
+                plugin.getPunishmentManager().unregisterPunishments();
+                plugin.getPunishmentManager().initializeDefault();
                 Chat.message(sender, "&eReloaded configuration...");
+            } else if (type.equalsIgnoreCase("info")) {
+
+                String infoMsg = String.format
+                        ("&7CombatLogger Plugin Info" +
+                                        "\n&fServer Version: &6%s \n" +
+                                        "&fPlugin Version: &6%s",
+                                Bukkit.getServer().getVersion(), plugin.getDescription().getVersion());
+
+                Chat.message(sender, infoMsg);
+
             } else {
-                Chat.message(sender, "&cInvalid arguments. Usage: /combatlogger reload");
+                Chat.message(sender, "&cInvalid arguments. Usage: /combatlogger [reload | info]");
             }
         } else {
-            Chat.message(sender, "&cInvalid arguments. Usage: /combatlogger reload");
+            Chat.message(sender, "&cInvalid arguments. Usage: /combatlogger [reload | info]");
         }
 
         return false;
@@ -46,6 +60,6 @@ public class CombatLoggerCommand implements CommandExecutor, TabCompleter {
     @Nullable
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        return List.of("reload");
+        return List.of("info", "reload");
     }
 }
