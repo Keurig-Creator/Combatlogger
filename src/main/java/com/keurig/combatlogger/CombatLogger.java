dde8961a5f5ee7579d5508f24d0e5e1bb3efa121
David <threetwoone112@gmail.com>
2024-08-02 23:13:50 -0500

    Improved Config System

--- src/main/java/com/keurig/combatlogger/CombatLogger.java
+++ src/main/java/com/keurig/combatlogger/CombatLogger.java
@@ -1,5 +1,6 @@
 package com.keurig.combatlogger;
 
+import com.google.common.base.Charsets;
 import com.keurig.combatlogger.command.CombatLoggerCommand;
 import com.keurig.combatlogger.handler.CombatPlayer;
 import com.keurig.combatlogger.listeners.*;
@@ -12,9 +13,13 @@ import com.keurig.combatlogger.utils.PlaceholderAPIHook;
 import com.keurig.combatlogger.utils.factions.FactionsManager;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class CombatLogger extends CombatPlugin {
@@ -56,6 +61,7 @@ public class CombatLogger extends CombatPlugin {
         PunishmentConfig punishmentConfig = new PunishmentConfig(this, new File(getDataFolder(), "punishments.yml"));
     }
 
+
     @Override
     public void onDisable() {
         this.punishmentManager.unregisterPunishments();
@@ -64,10 +70,18 @@ public class CombatLogger extends CombatPlugin {
 
     public void registerEvents() {
         Bukkit.getPluginManager().registerEvents(new AttackListener(), this);
-        Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
+        Bukkit.getPluginManager().registerEvents(new QuitListener(), this);
         Bukkit.getPluginManager().registerEvents(new DeathListener(), this);
         Bukkit.getPluginManager().registerEvents(new CommandListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new ProjectileListener(), this);
 
+        String[] versionComponents = Bukkit.getServer().getBukkitVersion().split("-")[0].split("\\.");
+        int majorVersion = Integer.parseInt(versionComponents[0]);
+        int minorVersion = Integer.parseInt(versionComponents[1]);
+
+        if (majorVersion > 1 || (majorVersion == 1 && minorVersion >= 9)) {
+            Bukkit.getPluginManager().registerEvents(new ElytraListener(), this);
+        }
         // Check if there are any regions, if not disable section
         List<String> regions = getConfig().getStringList("protected-regions.regions");
         if (!regions.isEmpty()) {
@@ -76,8 +90,18 @@ public class CombatLogger extends CombatPlugin {
     }
 
     public void registerConfig() {
-        getConfig().options().copyDefaults();
-        saveDefaultConfig();
+        InputStream inputStream = getResource("config.yml");
+        try {
+            if (inputStream != null) {
+                getConfig().setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(inputStream, Charsets.UTF_8)));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        deleteOld(getConfig());
+        getConfig().options().copyDefaults(true);
+        saveConfig();
     }
 
     public String replaceMsg(Player player, String message) {
@@ -86,5 +110,25 @@ public class CombatLogger extends CombatPlugin {
         }
 
         return message;
+    }
+
+    private void deleteOld(FileConfiguration config) {
+        String forceGamemode = config.getString("on-combat.force-gamemode");
+
+        if (config.get("on-combat.force-gamemode") instanceof String) {
+            config.set("on-combat.force-gamemode", null);
+        }
+
+        if (config.getString("on-combat.force-gamemode-message") != null) {
+            config.set("on-combat.force-gamemode-message", null);
+        }
+
+        if (config.get("chat.use") != null) {
+            config.set("chat.use", null);
+        }
+
+        if (config.get("actionbar.use") != null) {
+            config.set("actionbar.use", null);
+        }
     }
 }
--- src/main/java/com/keurig/combatlogger/utils/ConfigValue.java
+++ src/main/java/com/keurig/combatlogger/utils/ConfigValue.java
@@ -5,28 +5,63 @@ import org.bukkit.GameMode;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ConfigValue {
 
+    public static int COMBAT_TIMER;
+    public static boolean ACTIONBAR_ENABLED;
+    public static String ACTIONBAR_MESSAGE_ON;
+    public static String ACTIONBAR_MESSAGE_OFF;
+    public static boolean CHAT_ENABLED;
+    public static String CHAT_INTERVAL_MESSAGE;
+    public static String CHAT_MESSAGE_ON;
+    public static String CHAT_MESSAGE_OFF;
     public static GameMode FORCE_GAMEMODE;
     public static String FORCE_GAMEMODE_MSG;
-    public static Set<EntityType> IGNORED_PROJECTILES = new HashSet<EntityType>();
+    public static Set<EntityType> IGNORED_PROJECTILES;
+    public static Set<EntityType> BLOCKED_PROJECTILES;
+    public static List<Integer> CHAT_INTERVAL;
+    public static boolean DISABLE_ELYTRA;
 
     static {
+        IGNORED_PROJECTILES = new HashSet<>();
+        BLOCKED_PROJECTILES = new HashSet<>();
+        CHAT_INTERVAL = new ArrayList<>();
         loadValues();
     }
 
     public static void loadValues() {
         CombatLogger instance = CombatLogger.getInstance();
         FileConfiguration config = instance.getConfig();
+
+        BLOCKED_PROJECTILES.clear();
+        IGNORED_PROJECTILES.clear();
+        CHAT_INTERVAL.clear();
+
+        COMBAT_TIMER = config.getInt("combat-timer");
+
+        ACTIONBAR_ENABLED = config.getBoolean("actionbar.enabled");
+        ACTIONBAR_MESSAGE_ON = config.getString("actionbar.on-message");
+        ACTIONBAR_MESSAGE_OFF = config.getString("actionbar.off-message");
+
+        CHAT_ENABLED = config.getBoolean("chat.enabled");
+        CHAT_MESSAGE_ON = config.getString("chat.on-message");
+        CHAT_MESSAGE_OFF = config.getString("chat.off-message");
+
+        CHAT_INTERVAL = config.getIntegerList("chat.interval.seconds");
+        CHAT_INTERVAL_MESSAGE = config.getString("chat.interval.message");
+
         try {
-            FORCE_GAMEMODE = GameMode.valueOf(config.getString("on-combat.force-gamemode.mode", "SURVIVAL").toUpperCase());
+            FORCE_GAMEMODE = GameMode.valueOf(config.getString("on-combat.force-gamemode.mode").toUpperCase());
         } catch (IllegalArgumentException e) {
             FORCE_GAMEMODE = null;
         }
-        FORCE_GAMEMODE_MSG = config.getString("on-combat.force-gamemode.message", "&6Your gamemode has been updated.");
+        FORCE_GAMEMODE_MSG = config.getString("on-combat.force-gamemode.message");
+
 
         for (String entity : config.getStringList("ignored-projectiles")) {
             try {
@@ -34,5 +69,14 @@ public class ConfigValue {
             } catch (IllegalArgumentException ignored) {
             }
         }
+
+        for (String entity : config.getStringList("blocked-projectiles")) {
+            try {
+                BLOCKED_PROJECTILES.add(EntityType.valueOf(entity.toUpperCase()));
+            } catch (IllegalArgumentException ignored) {
+            }
+        }
+
+        DISABLE_ELYTRA = config.getBoolean("on-combat.disable-elytra");
     }
 }
