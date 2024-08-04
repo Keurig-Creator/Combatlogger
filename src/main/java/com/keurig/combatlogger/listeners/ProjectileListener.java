c3023bf9812126405ddee69b6605b8c63bdf1424
David <threetwoone112@gmail.com>
2024-08-03 21:50:08 -0500

    Update ProjectileListener.java

--- src/main/java/com/keurig/combatlogger/listeners/ProjectileListener.java
+++ src/main/java/com/keurig/combatlogger/listeners/ProjectileListener.java
@@ -1,5 +1,6 @@
 package com.keurig.combatlogger.listeners;
 
+import com.keurig.combatlogger.CombatLogger;
 import com.keurig.combatlogger.api.CombatLoggerAPI;
 import com.keurig.combatlogger.utils.ConfigValue;
 import org.bukkit.Material;
@@ -12,19 +13,15 @@ import org.bukkit.event.entity.ProjectileLaunchEvent;
 public class ProjectileListener implements Listener {
     @EventHandler
     public void onInteract(ProjectileLaunchEvent event) {
-        if (!ConfigValue.BLOCKED_PROJECTILES.isEmpty()) {
+        if (ConfigValue.DISABLE_ENDER_PEARLS && event.getEntity().getType() == EntityType.ENDER_PEARL) {
             if (event.getEntity().getShooter() instanceof Player player) {
                 if (!CombatLoggerAPI.isTagged(player))
                     return;
 
-                EntityType entityType = event.getEntityType();
-
-                if (ConfigValue.BLOCKED_PROJECTILES.contains(entityType)) {
-                    try {
-                        if (entityType == EntityType.ENDER_PEARL)
-                            player.setCooldown(Material.ENDER_PEARL, 0);
-                    } catch (NoSuchMethodError ignored) {
-                    }
+                if (CombatLogger.getInstance().spigotVersion >= 19) {
+                    player.setCooldown(Material.ENDER_PEARL, 0);
+                } else {
+                    player.getItemInHand().setAmount(player.getItemInHand().getAmount() + 1);
                 }
 
                 event.setCancelled(true);
