d86a045ba1ff04e7028a05ca5e43edf4ea996255
David <threetwoone112@gmail.com>
2026-01-13 11:36:46 -0600

    fixed death listener

--- src/main/java/com/keurig/combatlogger/listeners/DeathListener.java
+++ src/main/java/com/keurig/combatlogger/listeners/DeathListener.java
@@ -2,9 +2,9 @@ package com.keurig.combatlogger.listeners;
 
 import com.keurig.combatlogger.CombatLogger;
 import com.keurig.combatlogger.handler.CombatPlayer;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
@@ -12,25 +12,14 @@ public class DeathListener implements Listener {
 
     private final CombatLogger plugin = CombatLogger.getInstance();
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
         CombatPlayer combatPlayer = this.plugin.getCombatPlayer();
 
-        if (player.getKiller() == player) {
-            return;
-        }
-
-        if (player.getKiller() instanceof Player) {
-            combatPlayer.removePlayer(player);
-
-            String combatOffChat = this.plugin.config.getString("chat.off-message");
-
-            if (combatOffChat != null) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', combatOffChat));
-            }
-        } else if (player.getKiller() != null && !player.getKiller().getType().isSpawnable()) {
-
+        // Remove combat for the player who died
+        if (combatPlayer.isTagged(player)) {
+            combatPlayer.removeCombat(player);
         }
     }
 }
